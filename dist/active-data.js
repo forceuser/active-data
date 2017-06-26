f72b37d1a8b88c879d738bf3ed865c8e8d1368b5
forceuser <forceuserz@gmail.com>
2017-06-26 12:43:10 +0300

    1.0.21 release commit

--- dist/active-data.js
+++ dist/active-data.js
@@ -4,9 +4,9 @@
 	else if(typeof define === 'function' && define.amd)
 		define([], factory);
 	else if(typeof exports === 'object')
-		exports["active-data.js"] = factory();
+		exports["activeData"] = factory();
 	else
-		root["active-data.js"] = factory();
+		root["activeData"] = factory();
 })(this, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
--- webpack/build.config.js
+++ webpack/build.config.js
@@ -11,7 +11,7 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, "../dist"),
 		filename: "[name].js",
-		library: "[name].js",
+		library: "activeData",
 		libraryTarget: "umd"
 	},
 	devtool: "source-map",
